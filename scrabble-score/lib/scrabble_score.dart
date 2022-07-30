8af4958c4216bc8bc09a13288a5cd08c19664b4c
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-07-31 00:00:58 +0300

    added scrabble-score

---
+++ scrabble-score/lib/scrabble_score.dart
@@ -0,0 +1,44 @@
+int score(String s) {
+  int point = 0;
+  String inputWord = s.toUpperCase();
+  List<String> words = inputWord.split('');
+  var keys = {
+    'A': 1,
+    'E': 1,
+    'I': 1,
+    'O': 1,
+    'U': 1,
+    'L': 1,
+    'N': 1,
+    'R': 1,
+    'S': 1,
+    'T': 1,
+    'D': 2,
+    'G': 2,
+    'B': 3,
+    'C': 3,
+    'M': 3,
+    'P': 3,
+    'F': 4,
+    'H': 4,
+    'V': 4,
+    'W': 4,
+    'Y': 4,
+    'K': 5,
+    'J': 8,
+    'X': 8,
+    'Q': 10,
+    'Z': 10,
+  };
+
+  for (var word in words) {
+    if (keys.containsKey(word)) {
+      int kPoint = keys[word]!;
+
+      point += kPoint;
+    }
+  }
+  print(point);
+
+  return point;
+} //
